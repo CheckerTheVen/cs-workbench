9e1c46ecf42cb17241f89c01ae76b9943db7f687
CheckerTheVen <ac.cekic@outlook.com>
2023-06-06 00:21:41 +0200

    Implemented: Constructors and optional parameters

--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -1,81 +1,47 @@
 ﻿using System.Collections;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
 
 public class Coin
 {
-    public int Value;
+    public int Value = -1;
 
-    public Coin(int value)
+    public Coin(int value = 0)
     {
-        Value = value;
+        this.Value = value;
     }
 }
 
-public class Coins : IEnumerable
+public class Coins : IEnumerator, IEnumerable
 {
-    private Coin[] Coin;
-
-    public Coins(Coin[] initializer)
+    public Coins()
     {
-        Coin = new Coin[initializer.Length];
-
-        for (int i = 0; i < initializer.Length; i++)
-        {
-            Coin[i] = initializer[i];
-        }
+        
     }
 
-    public IEnumerator GetEnumerator()
-    {
-        return new CoinEnumerator(Coin);
-    }
-}
+	public object Current { get { throw new NotImplementedException(); } }
 
-public class CoinEnumerator : IEnumerator
-{
-    public Coin[] Coin;
+	public IEnumerator GetEnumerator()
+	{
+		throw new NotImplementedException();
+	}
 
-    public int Position = -1;
+	public bool MoveNext()
+	{
+		throw new NotImplementedException();
+	}
 
-    public CoinEnumerator(Coin[] initializer)
-    {
-        Coin = initializer;
-    }
-
-    bool IEnumerator.MoveNext()
-    {
-        Position++;
-        return (Position > Coin.Length);
-    }
-
-    void IEnumerator.Reset()
-    {
-        Position = -1;
-    }
-
-    object IEnumerator.Current{ get { return CurrentCoin; } }
-
-    public Coin CurrentCoin { get { try { return Coin[Position]; } catch (IndexOutOfRangeException) { throw new InvalidOperationException(); } } }
+	public void Reset()
+	{
+        throw new NotImplementedException();
+	}
 }
 
 public class Program
 {
     public static void Main()
     {
-        int count = 6;
-        Coin[] coin = new Coin[count];
-        
-        for (int current = 0; current < count; current++)
-        {
-            coin[current] = new Coin(5 * current % 3);
-        }
+        Coin a = new Coin();
+        Coin b = new Coin(64);
 
-        Coins coins = new Coins(coin);
-
-        foreach (Coin current in coins)
-        {
-            Console.WriteLine(current.Value.ToString());
-        }
+        Console.WriteLine(a + "\n\n" + b);
     }
 }
--- readme.md
+++ readme.md
@@ -5,5 +5,8 @@ This project serves as testing grounds for code, purely written in C#[^1].
 Currently testing:
 + Interfaces and inheritences
 + `Enumerable` interface
++ Constructors
++ Constructor overload
++ Optional parameters
 	
 [^1]: The code needs to be built and compiled first, as there are no binaries included.
